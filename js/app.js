de555d566aac59b40d65be2dc8d79388caca6c09
abc <abc@example.com>
2019-10-03 16:17:24 +0300

    Initial commit

--- js/app.js
+++ js/app.js
@@ -3,3 +3,26 @@ document.getElementById("cart-info").addEventListener("click", function() {
   cart.classList.toggle("show-cart");
   console.log(cart);
 });
+
+const t1 = new TimelineMax({ onUpdate: updatePercentage });
+const t12 = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+t1.from("#about", 0.5, { y: -200, opacity: 0 });
+t1.from("#box", 1, { x: 200, opacity: 0 });
+t1.from("#text", 1, { x: -200, opacity: 0 });
+t1.from("#btn", 1, { x: -200, opacity: 0 });
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".about",
+  triggerHook: "onLeave",
+  duration: "100%"
+})
+  .setPin(".about")
+  .setTween(t1)
+  .addTo(controller);
+
+function updatePercentage() {
+  t1.progress();
+  console.log(t1.progress());
+}
